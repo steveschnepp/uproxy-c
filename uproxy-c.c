d104e7b12d52209db20db491e4f6737c9a672cc2
Steve Schnepp <steve.schnepp@pwkf.org>
2017-03-15 21:20:53 +0100

    uproxy-c: enable compilation of unit

--- uproxy-c.c
+++ uproxy-c.c
@@ -10,10 +10,20 @@
 
 #define _POSIX_C_SOURCE 200809L
 
+#include <signal.h>
 #include <string.h>
+#include <unistd.h>
+
 #include <sys/stat.h>
 
 #include "lib/mkpath.h"
+
+/* Size constants */
+const size_t SIZE_X = 4;
+const size_t SIZE_S = 256;
+const size_t SIZE_M = 4 * 1024; // 4kiB
+const size_t SIZE_L = 64 * 1024; // 64kiB
+const size_t SIZE_H = 1024 * 1024; // 1MiB
 
 const int BUFFER_SIZE = 64 * 1024; // 64k buffer
 static char CACHEDIR[] = "cache"; // default is $(pwd)/cache
@@ -21,24 +31,29 @@ static int PORT = 3128;
 static int debug = 1;
 
 struct header_s {
-	char key[];
-	char value[];
-}
+	char key[SIZE_S];
+	char value[SIZE_M];
+};
 
 struct request_s {
-	char method[];
-	char url[];
-	char version[];
-}
+	char method[SIZE_S];
+	char url[SIZE_M];
+	char version[SIZE_X];
+};
 
 struct url_s {
-	char scheme[];
-	char host[];
+	char scheme[SIZE_S];
+	char host[SIZE_S];
 	int port;
 
-}
+};
 
-static void main(int argc, char** argv)
+struct request_s parse_request();
+struct header_s* parse_headers();
+struct url_s parse_url();
+void _connect(char*);
+
+int main(int argc, char** argv)
 {
 	// Make & go to CACHEDIR
 	mkpath(CACHEDIR);
@@ -53,23 +68,23 @@ static void main(int argc, char** argv)
 	// method CONNECT bypasses everything
 	if (strcmp("CONNECT", request.method) == 0) {
 		_connect(request.url);
-		return;
+		return 0;
 	}
 
 	// Support only GET
 	if (strcmp("GET", request.method) != 0) {
-		return;
+		return 0;
 	}
 
 	// Fetch the request headers
-	struct header_s headers[] = parse_headers();
+	struct header_s* headers = parse_headers();
 
 	// Parse the URL
 	struct url_s url = parse_url(request.url);
 
 	// Support only simple HTTP
 	if (strcmp("http", url.scheme) != 0) {
-		return;
+		return 0;
 	}
 
 
